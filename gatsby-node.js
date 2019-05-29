cb6860910028a958d550e361e11f1c9767711700
Mand Cashin <amanda.cashin@codecomputerlove.com>
2019-05-29 17:44:53 +0100

    add author posts page

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,6 +23,7 @@ exports.createPages = ({ actions, graphql }) => {
         tagsPage: path.resolve('src/templates/tags-page.js'),
         tagPosts: path.resolve('src/templates/tag-posts.js'),
         postList: path.resolve('src/templates/post-list.js'),
+        authorPosts: path.resolve('src/templates/author-posts.js'),
     }
 
     return graphql(`
@@ -109,5 +110,19 @@ exports.createPages = ({ actions, graphql }) => {
                 }
             })
         })
+
+        authors.forEach(author => {
+            createPage({
+                path: `/author/${slugify(author.name)}`,
+                component: templates.authorPosts,
+                context: {
+                    authorName: author.name,
+                    imageUrl: author.imageUrl,
+                },
+            })
+        })
+
+
+
     })
 }
